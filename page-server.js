0f00267b4e9a0bcc7c470e450d6fbd442e55b923
Nichola Baker <ndbaker1@outlook.com>
2021-02-14 14:16:33 -0600

    inital commit

---
+++ page-server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const path = require('path')
+const fs = require('fs')
+const axios = require('axios')
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/song', (req, res) => {
+	const id = axios.get('http://localhost:9000/id')
+	const time = axios.get('http://localhost:9000/time')
+	axios.all([id,time]).then(axios.spread((...responses) => {
+		res.send({
+			id: responses[0].data.id,
+			currentTime: responses[1].data.currentTime
+		})
+	}))
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ song-server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 9000
+const fs = require('fs')
+
+let currentSongId = ''
+let playbackStartTime = 0
+
+function playSong(songIndex) {
+	// read file and get duration
+	currentSongId = '1CU112QR3Lr81v32VaO9e6HLOOCi3FnjY'
+	const songDuration = 200 * 1000
+	playbackStartTime = Date.now()
+	setTimeout(() => {
+		playSong(0)
+	}, songDuration)
+}
+playSong(0)
+
+app.get('/id', (req, res) => {
+	res.send({ id: currentSongId })
+})
+
+app.get('/time', (req, res) => {
+	// convert from milliseconds to seconds
+	const songPosition = (Date.now() - playbackStartTime) / 1000 
+	res.send({ currentTime: songPosition })
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+})
