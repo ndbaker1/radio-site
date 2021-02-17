c3636ff5cb23f071d4fa0a94994b817529c95bb8
Nick Baker <ndbaker1@outlook.com>
2021-02-17 15:55:27 -0600

    added a credit link and a README

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,20 @@ export default function Home() {
       </Head>
       <main>
         < AudioStream />
+        < CreatorLink text="Nicholas Baker 2021" link="https://github.com/ndbaker1/radio-site" />
       </main>
     </div>
   )
+}
+
+function CreatorLink(props: { text: string, link: string }): JSX.Element {
+  return <a href={props.link} style={{
+    color: 'white',
+    position: "absolute",
+    right: 0,
+    bottom: 0,
+    margin: '6px',
+    fontSize: '0.8rem',
+    textDecoration: 'none'
+  }}>{props.text}</a>
 }
